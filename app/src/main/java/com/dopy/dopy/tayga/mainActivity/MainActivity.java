aaae9f17b1b692c241fd1ad485e096c054b5971e
jigin712 <dlwlrjs1@gmail.com>
2017-08-05 17:08:13 +0900

    주중 작업 완료

--- app/src/main/java/com/dopy/dopy/tayga/mainActivity/MainActivity.java
+++ app/src/main/java/com/dopy/dopy/tayga/mainActivity/MainActivity.java
@@ -4,8 +4,11 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,13 +19,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.TabHost;
 
 import com.dopy.dopy.tayga.R;
 import com.dopy.dopy.tayga.databinding.ActivityMainBinding;
 import com.dopy.dopy.tayga.databinding.AppBarMainBinding;
 import com.dopy.dopy.tayga.databinding.ContentMainBinding;
 
-import static com.dopy.dopy.tayga.R.id.mainFrame;
+import static android.R.id.toggle;
+import static com.dopy.dopy.tayga.R.id.toolbar;
 /*
 * 메인 액티비티에서 프래그먼트로 여러 인기순의 방송들을 정렬
 * */
@@ -35,26 +40,58 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+//        데이터 바인딩
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        appBarMainBinding = DataBindingUtil.findBinding(activityMainBinding.appBarMain.getRoot());
+        contentMainBinding = DataBindingUtil.findBinding(appBarMainBinding.contentMain.getRoot());
 
-//        TODO: 이 부분 databinding으로 처리하면 에러 발생
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+//       네비게이션 드로워의 동작을 바인딩
+        Toolbar toolbar = appBarMainBinding.toolbar;
         setSupportActionBar(toolbar);
         DrawerLayout drawer = activityMainBinding.drawerLayout;
-
-//        네비게이션 드로워의 동작을 바인딩
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
             this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
 //        네비게이션 드로워에 클릭 리스너 바인딩
         activityMainBinding.navView.setNavigationItemSelectedListener(this);
 
-//        최초 화면의 띄워주기 위해서
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.mainFrame,TwitchPopularBroadListFragment.newInstance());
-        fragmentTransaction.commit();
+//        커스텀 탭 추가  후 이미지 연결
+        TabLayout tabLayout = contentMainBinding.tabHostLayout;
+        View view1 = getLayoutInflater().inflate(R.layout.customtab,null);
+        view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.tiwich_logo);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(view1));
+        View view2 = getLayoutInflater().inflate(R.layout.customtab,null);
+        view2.findViewById(R.id.icon).setBackgroundResource(R.drawable.africatv_logo);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(view2));
+        View view3 = getLayoutInflater().inflate(R.layout.customtab,null);
+        view3.findViewById(R.id.icon).setBackgroundResource(R.drawable.youtube_logo);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(view3));
+
+//        뷰 페이저에 연결
+        final ViewPager viewPager = contentMainBinding.mainViewPager;
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager(),3);
+        viewPager.setAdapter(tabPagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+
+//      클릭 리스터 연결
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 //    백 버튼을 눌렀을 때 네비게이션 드로워가 닫히도록 하기위해서
--- app/src/main/java/com/dopy/dopy/tayga/mainActivity/TwitchPopularBroadListFragment.java
+++ app/src/main/java/com/dopy/dopy/tayga/mainActivity/TwitchPopularBroadListFragment.java
@@ -1,16 +1,21 @@
 package com.dopy.dopy.tayga.mainActivity;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dopy.dopy.tayga.R;
+import com.dopy.dopy.tayga.databinding.FragmentTwitchPopularBroadListBinding;
 
 public class TwitchPopularBroadListFragment extends Fragment {
+
+    FragmentTwitchPopularBroadListBinding binding;
 
     public TwitchPopularBroadListFragment() {
         // Required empty public constructor
@@ -31,5 +36,11 @@ public class TwitchPopularBroadListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_twitch_popular_broad_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentTwitchPopularBroadListBinding.bind(getView());
     }
 }
